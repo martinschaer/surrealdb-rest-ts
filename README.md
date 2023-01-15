0ccf6a40bbf0e62950384d9d7a1face869a913e7
Martin Schaer <martin@schaerweb.com>
2023-01-15 10:37:12 -0600

    Init

---
+++ README.md
@@ -0,0 +1,32 @@
+# SurrealDB REST client
+
+Stateless SurrealDB client built for serverless architectures
+
+## Motivation
+
+I built a Next.js app with next-auth and a [custom adapter](https://github.com/martinschaer/next-auth/tree/feature/adapter-surrealdb/packages/adapter-surrealdb) for SurrealDB. I deployed it on Vercel, and realized that I needed a stateless SureralDB client because the serverless nature of Vercel functions where losing my DB connection.
+
+To keep the custom adapter compatible with statefull backends, and benefit from a RPC connection, I built this library following the same function signatures as the official [Node.js driver for SurrealDB](https://surrealdb.com/docs/integration/libraries/nodejs).
+
+## To-do:
+
+- implement all methods:
+  - [ ] Sureral.Instance
+  - [ ] db.connect(url)
+  - [ ] db.wait()
+  - [ ] db.close()
+  - [ ] db.use(ns, db)
+  - [ ] db.signup(vars)
+  - [ ] db.signin(vars)
+  - [ ] db.invalidate()
+  - [ ] db.authenticate(token)
+  - [ ] db.let(key, val)
+  - [x] db.query(sql, vars)
+  - [x] db.select(thing)
+  - [x] db.create(thing, data)
+  - [ ] db.update(thing, data
+  - [x] db.change(thing, data)
+  - [ ] db.modify(thing, data)
+  - [x] db.delete(thing)
+- consider fetch-h2 instead of node-fetch
+- unit test with DB docker image
---
+++ src/index.ts
@@ -0,0 +1,115 @@
+import fetch from "node-fetch";
+import type { RequestInit } from "node-fetch";
+
+export type SurrealRESTResponse<T> = {
+  time: string;
+  status: "OK";
+  result: T;
+};
+
+export class SurrealREST {
+  url = "https://api.surrealdb.com/v1/sql";
+  ns: string | null = null;
+  db: string | null = null;
+  user: string | null = null;
+  password: string | null = null;
+
+  constructor(
+    url: string,
+    opts: { ns?: string; db?: string; user?: string; password?: string }
+  ) {
+    this.url = url;
+    this.ns = opts.ns || null;
+    this.db = opts.db || null;
+    this.user = opts.user || null;
+    this.password = opts.password || null;
+  }
+
+  private getFetchOpts(method: string, body?: string): RequestInit {
+    const headers: Record<string, string> = { Accept: "application/json" };
+    const opts: RequestInit = {
+      method,
+    };
+    if (this.ns) {
+      headers.NS = this.ns;
+    }
+    if (this.db) {
+      headers.DB = this.db;
+    }
+    if (this.user) {
+      headers.Authorization =
+        "Basic " +
+        Buffer.from(this.user + ":" + this.password, "utf-8").toString(
+          "base64"
+        );
+    }
+    opts.headers = headers;
+    if (body) {
+      opts.body = body;
+    }
+    return opts;
+  }
+
+  async select<T>(thing: string): Promise<T[]> {
+    const [table, id] = thing.split(":");
+    const res = await fetch(
+      `${this.url}/key/${table}/${id}`,
+      this.getFetchOpts("GET")
+    );
+    const json = (await res.json()) as SurrealRESTResponse<T[]>[];
+    if (!json[0]) throw new Error("Not found");
+    return json[0].result;
+  }
+
+  async query<T>(sql: string, vars?: Record<string, unknown>): Promise<T> {
+    const sqlWithVars = vars
+      ? sql.replace(/\$(\w+)/g, (_, key) => JSON.stringify(vars[key]))
+      : sql;
+    const res = await fetch(
+      `${this.url}/sql`,
+      this.getFetchOpts("POST", sqlWithVars)
+    );
+    const json = (await res.json()) as T;
+    return json;
+  }
+
+  async create<T extends Record<string, unknown>, U = T & { id: string }>(
+    table: string,
+    doc: T
+  ): Promise<U> {
+    const opts = this.getFetchOpts("POST", JSON.stringify(doc));
+    const res = await fetch(`${this.url}/key/${table}`, opts);
+    const json = (await res.json()) as SurrealRESTResponse<U[]>[];
+    if (!json[0] || !json[0].result[0]) throw new Error("Not found");
+    return json[0].result[0];
+  }
+
+  async change<T extends Record<string, unknown>>(
+    thing: string,
+    doc: T
+  ): Promise<T> {
+    const [table, id] = thing.split(":");
+    const res = await fetch(
+      `${this.url}/key/${table}/${id}`,
+      this.getFetchOpts("PATCH", JSON.stringify(doc))
+    );
+    const json = (await res.json()) as SurrealRESTResponse<T[]>[];
+    if (!json[0] || !json[0].result[0]) throw new Error("Not found");
+    return json[0].result[0];
+  }
+
+  async delete<T extends Record<string, unknown>, U = T & { id: string }>(
+    thing: string
+  ): Promise<U> {
+    const [table, id] = thing.split(":");
+    const res = await fetch(
+      `${this.url}/key/${table}/${id}`,
+      this.getFetchOpts("DELETE")
+    );
+    const json = (await res.json()) as SurrealRESTResponse<U>[];
+    if (!json[0]) throw new Error("Not found");
+    else {
+      return json[0].result;
+    }
+  }
+}
